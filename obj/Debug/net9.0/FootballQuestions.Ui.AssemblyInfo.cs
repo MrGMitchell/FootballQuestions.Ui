741c4b675ef52deda64a87f28e7b1f8137d00866
Glenn Mitchell <gmitchell818@gmail.com>
2025-06-08 22:48:20 -0400

    Updated website name

--- obj/Debug/net9.0/FootballQuestions.Ui.AssemblyInfo.cs
+++ obj/Debug/net9.0/FootballQuestions.Ui.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FootballQuestions.Ui")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22e987bc870c5955e97b5895706c6d4237a315d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a81268ee0ef295b85b8247b54946dbea8a0c4a20")]
 [assembly: System.Reflection.AssemblyProductAttribute("FootballQuestions.Ui")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FootballQuestions.Ui")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
