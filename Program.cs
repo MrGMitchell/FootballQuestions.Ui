db4d342c7a9ba8e3dcfaff5df7e4ca1597932951
Glenn Mitchell <gmitchell818@gmail.com>
2026-04-27 19:08:19 -0400

    Rollback

--- Program.cs
+++ Program.cs
@@ -3,8 +3,6 @@ using FootballQuestions.Ui.Services;
 using Microsoft.Net.Http.Headers;
 using Azure.Identity;
 using Microsoft.Extensions.Configuration.AzureAppConfiguration;
-using Microsoft.Azure.Cosmos;
-using Microsoft.AspNetCore.Components.Authorization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,43 +17,8 @@ builder.Configuration.AddAzureAppConfiguration(options =>
                refreshOptions.RegisterAll());
 });
 
-// Add authentication and authorization
-builder.Services.AddAuthentication("AppServiceAuth")
-    .AddScheme<AppServiceAuthenticationHandler.AppServiceAuthenticationOptions, AppServiceAuthenticationHandler>(
-        "AppServiceAuth", options => { });
-
-builder.Services.AddAuthorization();
-
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
-
-builder.Services.AddCascadingAuthenticationState();
-
-builder.Services.AddScoped<AuthenticationStateProvider, AppServiceAuthenticationStateProvider>();
-
-// User identity service - uses existing Cosmos DB
-builder.Services.AddScoped<IUserIdentityService>(serviceProvider =>
-{
-    var cosmosEndpoint = builder.Configuration.GetValue<string>("CosmosDbEndPoint")
-        ?? throw new InvalidOperationException("The setting `CosmosDbEndPoint` was not found.");
-    
-    var cosmosDatabaseKey = builder.Configuration.GetValue<string>("CosmosDbKey")
-        ?? throw new InvalidOperationException("The setting `CosmosDbKey` was not found.");
-
-    var userDatabaseId = builder.Configuration.GetValue<string>("FootballQuestionsDatabaseId")
-        ?? throw new InvalidOperationException("The setting `FootballQuestions` was not found.");
-
-    var userContainerId = builder.Configuration.GetValue<string>("FootballUserRolesContainerId")
-        ?? throw new InvalidOperationException("The setting `CosmosDbName` was not found.");
-
-    var cosmosClient = new CosmosClient(cosmosEndpoint, cosmosDatabaseKey);
-
-    var container = cosmosClient.GetContainer(userDatabaseId, userContainerId);
-    
-    var logger = serviceProvider.GetRequiredService<ILogger<UserIdentityService>>();
-
-    return new UserIdentityService(container, logger);
-});
 
 builder.Services.AddScoped<IFootballQuestionService, FootballQuestionService>();
 builder.Services.AddScoped<IEmailSubscriptionService, EmailSubscriptionService>();
@@ -63,8 +26,6 @@ builder.Services.AddScoped<IQuizAnswerService, QuizAnswerService>();
 builder.Services.AddScoped<IQuizReportService, QuizReportService>();
 
 builder.Services.AddAzureAppConfiguration();
-
-builder.Services.AddControllers();
 
 builder.Services.AddHttpClient("LunaApi", httpClient =>
 {
@@ -93,14 +54,9 @@ app.UseHttpsRedirection();
 
 app.UseAzureAppConfiguration();
 
-app.UseAuthentication();
-app.UseAuthorization();
-
 app.UseAntiforgery();
 
 app.MapStaticAssets();
-
-app.MapControllers();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
--- obj/Debug/net9.0/FootballQuestions.Ui.AssemblyInfo.cs
+++ obj/Debug/net9.0/FootballQuestions.Ui.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FootballQuestions.Ui")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb1ad8785bc00a3a723aecbe480aa9b0eae44462")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28557ed888a95ea1e6e29bf0677181c35ea2cb38")]
 [assembly: System.Reflection.AssemblyProductAttribute("FootballQuestions.Ui")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FootballQuestions.Ui")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
